771cdb5f156dc4473f7c3f4b6c9d9fd16c76ab7d
JoonKi-Kim <joonki402@gmail.com>
2021-07-08 22:49:54 +0900

    programmers level1 다트게임

---
+++ programmers/level1/23.py
@@ -0,0 +1,31 @@
+# 다트게임
+# https://programmers.co.kr/learn/courses/30/lessons/17682
+
+def solution(dartResult):
+    n = ''
+    score = []
+    for i in dartResult:
+        if i.isnumeric():
+            n += i
+        elif i == 'S':
+            n = int(n)
+            score.append(n)
+            n = ''
+        elif i == 'D':
+            n = int(n) ** 2
+            score.append(n)
+            n = ''
+        elif i == 'T':
+            n = int(n) ** 3
+            score.append(n)
+            n = ''
+        elif i == '*':
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+        elif i == '#':
+            score[-1] = score[-1] * -1
+
+    return sum(score)
