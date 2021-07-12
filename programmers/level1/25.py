d3584e24836500f3070a8bcd5272c24d8c7111f0
JoonKi-Kim <joonki402@gmail.com>
2021-07-12 22:55:06 +0900

    programmers level1 숫자 문자열과 영단어

---
+++ programmers/level1/25.py
@@ -0,0 +1,19 @@
+# 숫자 문자열과 영단어
+# https://programmers.co.kr/learn/courses/30/lessons/81301
+
+def solution(s):
+    answer = ''
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3',
+           'four': '4', "five": "5", "six": "6", "seven": "7",
+           "eight": "8", "nine": "9"}
+    tmp = ''
+    for a in s:
+        if a.isdigit():
+            answer += a
+        else:
+            tmp += a
+            if tmp in dic.keys():
+                answer += dic[tmp]
+                tmp = ''
+
+    return int(answer)
